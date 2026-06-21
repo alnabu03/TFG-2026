00dd0c784418d1d62ce708633879a922cea70a9e
Álvaro Navarro. <anavarrobudi@gmail.com>
2026-06-21 17:22:44 +0200

    archivos antiguos eliminados

--- servidor/console_input.py
+++
@@ -1,8 +0,0 @@
-import socket, json
-
-
-def leer_consola(cola):
-    print("Hilo de consola arrancado")
-    while True:
-        mensaje = input ("Escribe un mensaje para enviar al cliente: \n")
-        cola.put(mensaje)
